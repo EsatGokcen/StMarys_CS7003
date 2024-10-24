1a1e120a65b4d97e98dac9b2b44a73364ab48852
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-24 16:15:22 +0100

    import sqlalchemy, created engine and first table

--- week5/task1.py
+++ week5/task1.py
@@ -1,3 +1,22 @@
 #SQLAlchemy slide 19 
 # - you will practice how to create a table insert data, and query the data.
 
+from sqlalchemy import create_engine, inspect, MetaData, Table, Column, Integer, String
+
+engine = create_engine('sqlite://example.db', echo=False)
+metadata = MetaData()
+
+employees = Table('employees', metadata,
+                  Column('id',Integer, primary_key=True),
+                  Column('first_name', String),
+                  Column('last_name', String))
+
+metadata.create_all(engine)
+
+inspector = inspect(engine)
+tables = inspector.get_table_names()
+print(tables)
+
+
+
+
