68e05c5ff89d102c41d6e96e010932687fb46eb9
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-08 18:37:10 +0000

    added product data, created order data, task2

--- week7/task2_version_orm.py
+++ week7/task2_version_orm.py
@@ -64,3 +64,35 @@ customer4 = Customer(name = 'Brenda Willow', email = 'will@blue.com')
 
 session.add_all([customer1, customer2, customer3, customer4])
 session.commit()
+
+# Add products​
+product1 = Product(name='Laptop', price=852.5)
+product2 = Product(name='Smartphone', price=312.9)
+product3 = Product(name = 'Headset', price = 25.3)
+product4 = Product(name = 'Tablet', price = 150.0)
+
+session.add_all([product1, product2, product3, product4])
+session.commit()
+
+# Create orders​
+order1 = Order(customer=customer1)
+order1.products.append(product1)
+order1.products.append(product2)
+session.add(order1)
+
+order2 = Order(customer=customer2)
+order2.products.append(product3)
+order2.products.append(product2)
+session.add(order2)
+
+order3 = Order(customer=customer3)
+order3.products.append(product4)
+session.add(order3)
+
+order4 = Order(customer=customer4)
+order4.products.append(product1)
+order4.products.append(product2)
+order4.products.append(product4)
+session.add(order4)
+
+session.commit()
