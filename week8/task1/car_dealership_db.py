4ec2a6189318b3edec16c897e3abb122b0c3c1af
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 14:49:14 +0000

    create session, commit 5 Car objects to db

--- week8/task1/car_dealership_db.py
+++ week8/task1/car_dealership_db.py
@@ -14,6 +14,23 @@ class Cars(Base):
     make = Column(String, nullable=False)
     model = Column(String, nullable=False)
     year = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
-    mileage = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    mileage = Column(Float, nullable=False)
     date_added = Column(DateTime, nullable=False)
+
+# Create all tables in the engine​
+Base.metadata.create_all(engine)
+
+# Create a new session​
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Add customers​
+car1 = Cars(make = 'Toyota', model = 'Camry', year = 2018, price = 18000, mileage = 31000.00, date_added = '15/01/2023')
+car2 = Cars(make = 'Honda', model = 'Civic', year = 2019, price = 19500, mileage = 22000.00, date_added = '20/02/2023')
+car3 = Cars(make = 'Ford', model = 'Focus', year = 2017, price = 15000, mileage = 45000.00, date_added = '10/03/2023')
+car4 = Cars(make = 'Renault', model = 'Clio', year = 2021, price = 25000, mileage = 10000.00, date_added = '10/06/2024')
+car5 = Cars(make = 'Skoda', model = 'Octavia', year = 2020, price = 21500, mileage = 18000.00, date_added = '23/09/2024')
+
+session.add_all([car1, car2, car3, car4, car5])
+session.commit()
