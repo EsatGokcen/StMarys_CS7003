eb30d5f8859017491954351db5244bfdc99333db
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 14:39:40 +0000

    initial car class

--- week8/task1/car_dealership_db.py
+++ week8/task1/car_dealership_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import create_engine, Table, Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 # Create an engine that stores data in the local directory's ecommerce.db file
@@ -6,3 +6,14 @@ engine = create_engine('sqlite:///car_dealership.db')
 
 # Base class for classes definitions
 Base = declarative_base()
+
+# Define the customer class
+class Cars(Base):
+    __tablename__ = 'cars'
+
+    make = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+    mileage = Column(Integer, nullable=False)
+    date_added = Column(DateTime, nullable=False)
