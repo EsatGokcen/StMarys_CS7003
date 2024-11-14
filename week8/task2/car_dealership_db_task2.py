7fd90e6cd9cc8e03fcf40852180af4061dd9c69a
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 16:05:53 +0000

    initial task2 database file

--- week8/task2/car_dealership_db_task2.py
+++ week8/task2/car_dealership_db_task2.py
@@ -20,6 +20,20 @@ class Cars(Base):
     mileage = Column(Float, nullable=False)
     date_added = Column(String, nullable=False)
 
+# Define the salespersons class
+class SalesPersons(Base):
+    __tablename__ = 'salespersons'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False)
+
+# Define the sales class
+class Sales(Base):
+    __tablename__ = 'sales'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+
+
 # Create all tables in the engine​
 Base.metadata.create_all(engine)
 
@@ -27,7 +41,7 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Add Cars
+# Add Cars 
 car1 = Cars(make = 'Toyota', model = 'Camry', year = 2018, price = 18000, mileage = 31000.00, date_added = '15/01/2023')
 car2 = Cars(make = 'Honda', model = 'Civic', year = 2019, price = 19500, mileage = 22000.00, date_added = '20/02/2023')
 car3 = Cars(make = 'Ford', model = 'Focus', year = 2017, price = 15000, mileage = 45000.00, date_added = '10/03/2023')
@@ -36,20 +50,6 @@ car5 = Cars(make = 'Skoda', model = 'Octavia', year = 2020, price = 21500, milea
 
 session.add_all([car1, car2, car3, car4, car5])
 session.commit()
-
-# Query the data:
-# Toyota cars manufactured after 2015 with price less than £20000
-
-specific_cars1 = session.query(Cars).filter(and_(Cars.make == 'Toyota', Cars.year > 2015, Cars.price < 20000))
-for car in specific_cars1:
-    print(car.make, car.model, car.year, car.price, car.mileage, car.date_added)
-
-# Toyota or Honda cars with price less than £20000 and mileage below 50000
-# Cars added to the database between 1st January 2023 and 31st December 2023
-# Cars with mileage between 10000 and 50000 ​​
-# Cars where the model's name contains 'Cl’
-# Cars where the price is not null
-# Cars that either Toyota, Honda, or Ford
 
 # Remove the Database after Queries complete.
 
