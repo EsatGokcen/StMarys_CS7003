026ad86a601eeb0621eadcf072d31ccfaefc5875
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 15:53:04 +0000

    initial task2 subdir

---
+++ week8/task2/__init__.py
@@ -0,0 +1,13 @@
+# Car Dealership database
+
+# Now has two more tables: salespersons and sales
+
+# The table “Salespersons” should contain the following names: 
+# Callum Jones, Malcom Brenner, Jennifer Finnegan, and Martin Yates 
+
+# Define the relationships between tables
+
+# Sort cars by price in ascending order
+# Sort cars by make (ascending), year (descending), and price (ascending)
+# Sort cars by custom order: Toyota cars are given the highest priority (1), followed by Honda cars (2), and all other makes (3)
+# Sort cars based on the number of sales made by salespersons
