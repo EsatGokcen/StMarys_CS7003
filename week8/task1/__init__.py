f5c9bc5884c9996bb5c7df6e5b362888cfa64ccc
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 14:33:12 +0000

    initial week8, task1 dirs

---
+++ week8/task1/__init__.py
@@ -0,0 +1,9 @@
+# SQL Alchemy
+# Local Car Dealer Data Query:
+# Toyota cars manufactured after 2015 with price less than £20000
+# Toyota or Honda cars with price less than £20000 and mileage below 50000
+# Cars added to the database between 1st January 2023 and 31st December 2023
+# Cars with mileage between 10000 and 50000 ​​
+# Cars where the model's name contains 'Cl’
+# Cars where the price is not null
+# Cars that either Toyota, Honda, or Ford
